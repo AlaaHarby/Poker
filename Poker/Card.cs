b0f0af4b15094fe328283537d45ec87d5dc79f9e
Alaa <alaa.harby@gmail.com>
2016-11-15 11:05:25 -0800

    adding new classes

---
+++ Poker/Card.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker
+{
+    class Card : IComparable
+    {
+        char suit;
+        int rank;
+
+        public Card(char suit, int rank)
+        {
+            this.suit = suit;
+            this.rank = rank;
+        }
+
+        public char getSuit() { return suit; }
+        public int getRank() { return rank; }
+
+        public int CompareTo(object obj)
+        {
+            return (rank - (obj as Card).getRank());
+        }
+    }
+}
